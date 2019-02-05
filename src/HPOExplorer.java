f8f80bbdf2604ef773f861b35463f95332cf1603
Jacob Laframboise <jlaframboise@cs.queensu.ca>
2019-02-04 23:52:37 -0500

    I implemented a class to hold the data for each item, and made a method in main to read the file and create many item objects and hold them in an arrayList. I made a method in item to print out its info, and called it on all the loaded item. It appears to be successful and working.

--- src/HPOExplorer.java
+++ src/HPOExplorer.java
@@ -1,2 +1,93 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+
 public class HPOExplorer {
+
+    private static ArrayList<Item> items = new ArrayList<>();
+
+    private static void loadData(String fileName) {
+        try {
+            BufferedReader reader = Files.newBufferedReader(Paths.get(fileName));
+            String line = null;
+            int count = 0;
+            while ((line = reader.readLine()) != null) {
+                if (line.equals("[Term]")) {
+                    System.out.println("Calling loadItem");
+                    items.add(loadItem(reader));
+                }
+                count++;
+            }
+        } catch (IOException ie) {
+            ie.printStackTrace();
+        } finally {
+            System.out.println("done.");
+        }
+
+    }
+
+    private static Item loadItem(BufferedReader reader) {
+        try {
+            String line = null;
+            String id = reader.readLine();
+            String name = reader.readLine();
+            Item t = new Item(id, name);
+            while ((line = reader.readLine()) != null && !line.equals("")) {
+                if (line.substring(0, 7).equals("comment")) {
+                    t.addComment(line.substring(8));
+                } else if (line.substring(0, 6).equals("alt_id")) {
+                    t.addAlt_id(line.substring(7));
+                } else if (line.substring(0, 8).equals("consider")) {
+                    t.addConsider(line.substring(9));
+                } else if (line.substring(0, 10).equals("created_by")) {
+                    t.addCreated_by(line.substring(11));
+                } else if (line.substring(0, 13).equals("creation_date")) {
+                    t.addCreation_date(line.substring(14));
+                } else if (line.substring(0, 4).equals("is_a")) {
+                    t.addIs_a(line.substring(5));
+                } else if (line.substring(0, 12).equals("is_anonymous")) {
+                    t.addIs_anonymous(line.substring(13));
+                } else if (line.substring(0, 11).equals("is_obsolete")) {
+                    t.addIs_obsolete(line.substring(12));
+                } else if (line.substring(0, 14).equals("property_value")) {
+                    t.addProperty_value(line.substring(15));
+                } else if (line.substring(0, 11).equals("replaced_by")) {
+                    t.addReplaced_by(line.substring(12));
+                } else if (line.substring(0, 6).equals("subset")) {
+                    t.addSubset(line.substring(7));
+                } else if (line.substring(0, 7).equals("synonym")) {
+                    t.addSynonym(line.substring(8));
+                } else if (line.substring(0, 4).equals("xref")) {
+                    t.addXref(line.substring(5));
+                } else if (line.substring(0, 3).equals("def")) {
+                    t.addDef(line.substring(4));
+                } else {
+                    System.out.println("There is another attr type!!!!!!!!!!!!!!");
+                }
+
+            }
+            return t;
+        } catch (IOException ie) {
+            ie.printStackTrace();
+        } finally {
+            System.out.println("done Item");
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        loadData("C:\\_Root Folder\\ComputerPrograming\\HPO-Explorer\\res\\HPO.txt");
+
+        for (Item i : items.subList(0, 50)){
+            i.printData();
+        }
+
+    }
+
+
 }
--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,216 @@
+import java.util.ArrayList;
+import java.util.Map;
+
 public class Item {
+    // class variables ---------------------------------------------------------
+    private String id = null;
+
+
+    private String name = null;
+    private ArrayList<String> def = new ArrayList<>();
+    private ArrayList<String> alt_id = new ArrayList<>();
+    private ArrayList<String> comment = new ArrayList<>();
+    private ArrayList<String> consider = new ArrayList<>();
+    private ArrayList<String> created_by = new ArrayList<>();
+    private ArrayList<String> creation_date = new ArrayList<>();
+    private ArrayList<String> is_a = new ArrayList<>();
+    private ArrayList<String> is_anonymous = new ArrayList<>();
+    private ArrayList<String> is_obsolete = new ArrayList<>();
+    private ArrayList<String> property_value = new ArrayList<>();
+    private ArrayList<String> replaced_by = new ArrayList<>();
+    private ArrayList<String> subset = new ArrayList<>();
+    private ArrayList<String> synonym = new ArrayList<>();
+    private ArrayList<String> xref = new ArrayList<>();
+
+    // constructors ------------------------------------------------------------
+
+    public Item(String id, String name) {
+        setId(id);
+        setName(name);
+    }
+
+
+    // accessors ---------------------------------------------------------------
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<String> getDef() {
+        return def;
+    }
+
+    public ArrayList<String> getAlt_id() {
+        return alt_id;
+    }
+
+    public ArrayList<String> getComment() {
+        return comment;
+    }
+
+    public ArrayList<String> getConsider() {
+        return consider;
+    }
+
+    public ArrayList<String> getCreated_by() {
+        return created_by;
+    }
+
+    public ArrayList<String> getCreation_date() {
+        return creation_date;
+    }
+
+    public ArrayList<String> getIs_a() {
+        return is_a;
+    }
+
+    public ArrayList<String> getIs_anonymous() {
+        return is_anonymous;
+    }
+
+    public ArrayList<String> getIs_obsolete() {
+        return is_obsolete;
+    }
+
+    public ArrayList<String> getProperty_value() {
+        return property_value;
+    }
+
+    public ArrayList<String> getReplaced_by() {
+        return replaced_by;
+    }
+
+    public ArrayList<String> getSubset() {
+        return subset;
+    }
+
+    public ArrayList<String> getSynonym() {
+        return synonym;
+    }
+
+    public ArrayList<String> getXref() {
+        return xref;
+    }
+
+    public void printData(){
+        System.out.println(getId());
+        System.out.println(getName());
+        if (getDef().size()>0){
+            System.out.println(getDef());
+        }
+        if (getAlt_id().size()>0) {
+            System.out.println(getAlt_id());
+        }
+        if (getComment().size()>0) {
+            System.out.println(getComment());
+        }
+        if (getConsider().size()>0) {
+            System.out.println(getConsider());
+        }
+        if (getCreated_by().size()>0) {
+            System.out.println(getCreated_by());
+        }
+        if (getCreation_date().size()>0) {
+            System.out.println(getCreation_date());
+        }
+        if (getIs_a().size()>0) {
+            System.out.println(getIs_a());
+        }
+        if (getIs_anonymous().size()>0) {
+            System.out.println(getIs_anonymous());
+        }
+        if (getIs_obsolete().size()>0) {
+            System.out.println(getIs_obsolete());
+        }
+        if (getProperty_value().size()>0) {
+            System.out.println(getProperty_value());
+        }
+        if (getReplaced_by().size()>0) {
+            System.out.println(getReplaced_by());
+        }
+        if (getSubset().size()>0) {
+            System.out.println(getSubset());
+        }
+        if (getSynonym().size()>0) {
+            System.out.println(getSynonym());
+        }
+        if (getXref().size()>0) {
+            System.out.println(getXref());
+        }
+
+
+
+    }
+
+    // mutators ----------------------------------------------------------------
+
+    public void setId(String id) {
+        if (id != null) {
+            this.id = id;
+        }
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addDef(String def) {
+        this.def.add(def);
+    }
+
+    public void addAlt_id(String alt_id) {
+        this.alt_id.add(alt_id);
+    }
+
+    public void addComment(String comment) {
+        this.comment.add(comment);
+    }
+
+    public void addConsider(String consider) {
+        this.consider.add(consider);
+    }
+
+    public void addCreated_by(String created_by) {
+        this.created_by.add(created_by);
+    }
+
+    public void addCreation_date(String creation_date) {
+        this.creation_date.add(creation_date);
+    }
+
+    public void addIs_a(String is_a) {
+        this.is_a.add(is_a);
+    }
+
+    public void addIs_anonymous(String is_anonymous) {
+        this.is_anonymous.add(is_anonymous);
+    }
+
+    public void addIs_obsolete(String is_obsolete) {
+        this.is_obsolete.add(is_obsolete);
+    }
+
+    public void addProperty_value(String property_value) {
+        this.property_value.add(property_value);
+    }
+
+    public void addReplaced_by(String replaced_by) {
+        this.replaced_by.add(replaced_by);
+    }
+
+    public void addSubset(String subset) {
+        this.subset.add(subset);
+    }
+
+    public void addSynonym(String synonym) {
+        this.synonym.add(synonym);
+    }
+
+    public void addXref(String xref) {
+        this.xref.add(xref);
+    }
 }
